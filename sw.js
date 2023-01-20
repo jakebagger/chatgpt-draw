18c7bf7c2ad2d90d8ab2df353bb800569fdc42b1
jakebagger <jakebagger@gmail.com>
2023-01-19 16:09:28 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,46 @@
-self.addEventListener('install', function(event) {
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('canvas-drawing-v1')
-    .then(function(cache) {
-      return cache.addAll([
-        '/',
-        'index.html',
-        'icon.png',
-        'manifest.json'
-      ]);
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-    .then(function(response) {
-      if (response) {
-        return response;
-      }
-
-      return fetch(event.request);
-    })
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
+});
+
+self.addEventListener('message', event => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
